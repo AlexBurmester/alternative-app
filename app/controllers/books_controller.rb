61ffa186ebe67d0e39e01c173ff479404782970d
KanyeBest <burmester90@gmail.com>
2016-09-08 23:59:02 +0200

    Add find_book method to books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,16 @@
 class BooksController < ApplicationController
+	before_action :find_book, only: [:show]
 
 	def index
 		@books = Book.all.order("created_at DESC")
 	end
 
 	def show
-		@book = Book.find(params[:id])
 	end
+
+	private
+
+		def find_book
+			@book = Book.find(params[:id])
+		end
 end
