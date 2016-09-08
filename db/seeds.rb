af8394f2c1182a4051791b731b910c1a9f0579e6
KanyeBest <burmester90@gmail.com>
2016-09-09 00:17:28 +0200

    Add books to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+social_books = [
+	["Helhedssyn i socialt arbejde", "Margit Harder"],
+	["Den gode opgave", "Lotte Rienecker"],
+	["Nye horisonter i socialt arbejde", "Maria Appel Nissen"],
+	["Kommunikation i praksis", "Tom Eide"],
+	["Kommunikation og samarbejde", "Mads Hermansen"],
+	["Modeller i socialt arbejde", "Siv Oltedal"]
+]
+
+social_books.each do |title, author|
+	Book.create(title: title, author: author)
+end
