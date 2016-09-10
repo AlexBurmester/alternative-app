84d8f05c060fe9a926b930f206ebb148f17b3610
KanyeBest <burmester90@gmail.com>
2016-09-10 23:34:04 +0200

    Add book ratings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909232414) do
+ActiveRecord::Schema.define(version: 20160910202723) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at",            null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160909232414) do
     t.string   "book_img_content_type"
     t.integer  "book_img_file_size"
     t.datetime "book_img_updated_at"
+    t.integer  "rating"
   end
 
 end
