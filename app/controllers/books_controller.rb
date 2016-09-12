d32768d161c5cd0962214af3ea1ff8007e0c3592
KanyeBest <burmester90@gmail.com>
2016-09-12 16:13:23 +0200

    Add create book view/action with simple form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,18 +9,24 @@ class BooksController < ApplicationController
 	end
 
 	def new
+		@book = Book.new
 	end
 
 	def edit
 	end
 
 	def create
+		@book = Book.new(book_params)
 	end
 
 	def update
 	end
 
 	private
+
+		def book_params
+			params.require(:book).permit(:title, :author, :description, :rating, :verdict)
+		end
 
 		def find_book
 			@book = Book.find(params[:id])
