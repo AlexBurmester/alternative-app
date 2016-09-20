14cc45e4bf0882f86d58c9731c44041d72e21ddf
KanyeBest <burmester90@gmail.com>
2016-09-21 01:53:09 +0200

    Fix page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	#returns the full title on a per-page basis
 	def full_title(page_title = "")
-		base_title = "Alterisp"
+		base_title = "Pensumguiden"
 		if page_title.empty?
 			base_title
 		else
