4f86a2719e4bab3f5e1bf7d728d9b393543b8bd0
KanyeBest <burmester90@gmail.com>
2016-09-25 19:20:47 +0200

    Add meta tags and description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,21 @@ module ApplicationHelper
 
 	#returns the full title on a per-page basis
 	def full_title(page_title = "")
-		base_title = "Pensumguiden"
+		base_title = "Pensum Popo"
 		if page_title.empty?
 			base_title
 		else
 			page_title + " | " + base_title
 		end
 	end
+
+	def description(page_description = "")
+  	base_description = "Pensum Popo giver dig svaret."
+  	if page_description.empty?
+  		base_description
+  	else
+  		page_description + " " + base_description
+  	end
+	end
+
 end
